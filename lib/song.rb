c10dbe3f8797594659210a040a1399c0b4d0ae0d
Steve Anderson <prototype909@msn.com>
2020-05-14 03:32:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,19 @@
 class Song
   attr_accessor :name, :artist, :genre
- def initialize
+
+  @@count = 0
+  @@genere = []
+  @@artist = []
+  
+ def initialize(song, artist, genre)
+   @name = song
+   @artist = artist
+   @genere = genre
+   @@count += 1 
+   @@genere << genre
+   @@artist << artist
  end
+
+ def name(name)
+
 end
