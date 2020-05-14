c1281c05e0d42e8283e091d7560d32a0856cc227
Steve Anderson <prototype909@msn.com>
2020-05-14 04:18:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,7 +39,7 @@ class Song
   def self.artist_count
     artists_hash = Hash.new(0)
     @@artists.each do |artist|
-      artists_hash [artist] += 1
+      artists_hash[artist] += 1
     end
     artists_hash
    end
